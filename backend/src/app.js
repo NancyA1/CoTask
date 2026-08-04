37fcd2103e2c6d10b3014da51cba5bd636e07fd5
NancyA1 <nancyashraff101@gmail.com>
2026-08-04 14:49:49 +0300

    feat: add project router

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,8 @@ const express = require("express"); //import express
 const app = express(); // creating a express application
 
 app.use(express.json());
+const projectRoutes = require("./routes/projectRoutes");
+app.use("/api/projects",projectRoutes);
 
 app.get("/api/healthy",(req,res)=>{
    res.json({
