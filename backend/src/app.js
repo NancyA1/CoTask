431b07b4b62dc7b88daea91128f3e34ced7e45e1
NancyA1 <nancyashraff101@gmail.com>
2026-08-04 14:18:37 +0300

    feat: add json body parsing middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,13 @@ const express = require("express"); //import express
 
 const app = express(); // creating a express application
 
+app.use(express.json());
+
 app.get("/api/healthy",(req,res)=>{
    res.json({
     message:"CoTask API is running"
    })
 });
-
 
 
 module.exports=app;
